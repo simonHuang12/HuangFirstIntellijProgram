7a6f5a2b1aa56418a765a265085c71eb582ca6a5
simonHuang12 <simonh8@nycstudents.net>
2022-09-28 08:40:30 -0400

    added print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("1`12`1312314124141412412");
+    }
 }
